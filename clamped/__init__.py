b87466e92f3285af8117b409139669095d07e08c
Jim Baker <jim.baker@rackspace.com>
2014-01-20 23:17:42 -0700

    Use clamp_base metaclass factory

--- clamped/__init__.py
+++ clamped/__init__.py
@@ -1,13 +1,13 @@
-import java
 from java.io import Serializable
 from java.util.concurrent import Callable
 
-from clamp import ClampProxyMaker
+from clamp import clamp_base
 
 
-class BarClamp(Callable, Serializable):
+BarBase = clamp_base("bar")
 
-    __proxymaker__ = ClampProxyMaker("bar")
+
+class BarClamp(BarBase, Callable, Serializable):
 
     def __init__(self):
         print "Being init-ed", self
