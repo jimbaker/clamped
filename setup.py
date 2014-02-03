59b7560853993dc96d55b8dff37981266ea57d20
Jim Baker <jim.baker@rackspace.com>
2014-02-03 07:12:28 -0700

    Remap setup.py install command to clamp command to simplify installation

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ import ez_setup
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
-
+from clamp.commands import clamp_command
 
 setup(
     name = "clamped",
     version = "0.1",
     packages = find_packages(),
-    install_requires = ["clamp>=0.3"],
+    install_requires = ["clamp>=0.4"],
     clamp = {
-        "modules": ["clamped"]},
+        "modules": ["clamped"]
+    },
+    cmdclass = { "install": clamp_command }
 )
